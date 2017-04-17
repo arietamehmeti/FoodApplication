daf9c187305a6cdb5859d77021185735cf11a49b
arietamehmeti <arietamehmeti@gmail.com>
2017-04-17 22:35:17 +0200

    Add service class for testing

--- app/src/main/java/com/example/hp/foodapplication/MainActivity.java
+++ app/src/main/java/com/example/hp/foodapplication/MainActivity.java
@@ -1,13 +1,99 @@
 package com.example.hp.foodapplication;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CursorAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.hp.foodapplication.db.MealsNetworkContract;
+import com.example.hp.foodapplication.db.MealsNetworkDb;
+import com.example.hp.foodapplication.services.MealTypesService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivity";
+
+    // Define a projection that specifies which columns from the database
+    // you will actually use after this query.
+    // To save on resources only return the column values that you actually need.
+    private static final String[] PROJECTION = {
+            MealsNetworkContract.MealType._ID,
+            MealsNetworkContract.MealType.COLUMN_MEALTYPE_TITLE
+    };
+
+    private final static String[] FROM_COLUMNS = {
+            MealsNetworkContract.MealType.COLUMN_MEALTYPE_TITLE
+     };
+
+    private final static int[] TO_IDS = {
+            R.id.tv_meal_title,
+     };
+
+    // How you want the results sorted in the resulting Cursor
+    private static final String SORT_ORDER = MealsNetworkContract.MealType.COLUMN_MEALTYPE_PRIORITY + " ASC";
+
+
+    private MealsNetworkDb dbHelper;
+    private CursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView( R.layout.activity_main);
+        dbHelper = new MealsNetworkDb(this);
+        Log.d(LOG_TAG, "on create ");
+
+        getAllMealTypes();
+
+        adapter = new SimpleCursorAdapter(this, R.layout.meal_type_layout, null, FROM_COLUMNS, TO_IDS, 0);
+        ((SimpleCursorAdapter) adapter).setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if (view.getId() != R.id.tv_meal_title) {
+                    return false;
+                }
+                return true;
+            }
+
+            });
+    }
+
+    private void getAllMealTypes() {
+//        SQLiteDatabase db = dbHelper.getReadableDatabase();
+//
+//        Cursor cursor = db.query(
+//                MealsNetworkContract.MealType.TABLE_NAME,           // The table to query
+//                PROJECTION,                                             // The columns to return
+//                null,                                                   // The columns for the WHERE clause
+//                null,                                                   // The values for the WHERE clause
+//                null,                                                   // don't group the rows
+//                null,                                                   // don't filter by row groups
+//                SORT_ORDER                                              // The sort order
+//        );
+//
+//        int titleColumn = cursor.getColumnIndexOrThrow(MealsNetworkContract.MealType.COLUMN_MEALTYPE_TITLE);
+//        while (cursor.moveToNext()) {
+//            String title = cursor.getString(titleColumn);
+//        }
+//
+//        TextView resultsTextView = (TextView) findViewById(R.id.tv_meal_title);
+//
+//        cursor.close();
+
+        Intent intent = new Intent(this, MealTypesService.class);
+        intent.setAction(MealTypesService.ACTION_GET_MEAL_TYPES);
+        Log.d(LOG_TAG, "getAllMeal types");
+        Log.d(LOG_TAG, "" +MealTypesService.ACTION_GET_MEAL_TYPES);
+
+        startService(intent);
     }
 }
---
+++ app/src/main/java/com/example/hp/foodapplication/services/MealTypesService.java
@@ -0,0 +1,100 @@
+package com.example.hp.foodapplication.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Hp on 4/17/2017.
+ */
+
+public class MealTypesService extends IntentService {
+
+    public static final String ACTION_GET_MEAL_TYPES = "com.example.hp.foodapplication.services.GET_MEAL_TYPES";
+
+    public static final String ACTION_GET_MEAL_TYPES_RESULT = "com.example.hp.foodapplication.services.GET_MEAL_TYPES_RESULT";
+
+    private static final String LOG_TAG = "StudentService";
+
+    public static final String EXTRA_MEAL_TYPES_RESULT = "students.result";
+
+    private static final String GET_MEAL_TYPES_URL = "http://clubs-sdmdcity.rhcloud.com/rest/types";
+
+    public MealTypesService() {
+        super("Meal Types Service");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d(LOG_TAG, "onhandle intent");
+
+        String action = intent.getAction();
+        if (ACTION_GET_MEAL_TYPES.equals(action)) {
+            Log.d(LOG_TAG, "" +action);
+
+            getMealTypes(intent);
+        } else {
+            throw new UnsupportedOperationException("No implementation for action " + action);
+        }
+    }
+
+    private void getMealTypes(Intent intent){
+        InputStream is = null;
+
+        try {
+            URL url = new URL(GET_MEAL_TYPES_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            // Starts the query
+            conn.connect();
+
+            int response = conn.getResponseCode();
+            is = conn.getInputStream();
+            Log.d(LOG_TAG, "The response is: " + response);
+
+            // Convert the InputStream into a bitmap
+            String result = convertStreamToString(is);
+
+            Intent resultIntent = new Intent(ACTION_GET_MEAL_TYPES_RESULT);
+            resultIntent.putExtra(EXTRA_MEAL_TYPES_RESULT, result);
+
+            LocalBroadcastManager.getInstance(this).sendBroadcast(resultIntent);
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Exception fetching students", e);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG, "Exception closing stream", e);
+                }
+            }
+        }
+    }
+
+    private String convertStreamToString(InputStream is) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length = 0;
+        while ((length = is.read(buffer)) != -1) {
+            baos.write(buffer, 0, length);
+        }
+
+        return baos.toString();
+    }
+}
